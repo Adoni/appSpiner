bc3b17a0d79d13d39bc07a3685ee6596cd335d54
Adoni <yiyufafa@gmail.com>
2013-01-06 15:04:02 +0800

    add Readme.mk

---
+++ README.mk
@@ -0,0 +1,5 @@
+#appSpider
+##This is a simple spider to crawl information of applications in iTunes, using scrapy.
+##这是一个基于scrapy的简单爬虫应用，目的是从iTunes上抓取所有应用的信息，包括名称、开发商、icon、评价等
+##If you are interested in scrapy or crawling some interested thing in the Internet, welcome to work together 
+##如果你也对爬虫或者scrapy感兴趣或者想从网上爬一些好玩的东西，欢迎一起工作:)
